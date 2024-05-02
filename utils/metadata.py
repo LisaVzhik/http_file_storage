0bd08ffebd4186304a73049ea20d331bb97bd3ff
LisaVzhik <kovalevaec@gmail.com>
2024-05-02 13:54:20 +0300

    Add some typyhints

--- utils/metadata.py
+++ utils/metadata.py
@@ -1,7 +1,7 @@
 from json import load, dump
 
 
-def load_metadata():
+def load_metadata() -> dict:
     """ Загрузка метаданных из файла, если файла нет, возвращает пустой словарь """
     try:
         with open('file_metadata.json', 'r') as file:
@@ -10,20 +10,20 @@ def load_metadata():
         return {}
 
 
-def save_metadata(metadata):
+def save_metadata(metadata: dict) -> None:
     """ Сохранение метаданных в файл """
     with open('file_metadata.json', 'w') as file:
         dump(metadata, file, indent=4)
 
 
-def add_file_metadata(file_hash, username, file_path):
+def add_file_metadata(file_hash: str, username: str, file_path: str) -> None:
     """ Добавление метаданных для нового файла """
     metadata = load_metadata()
     metadata[file_hash] = {"owner": username, "path": file_path}
     save_metadata(metadata)
 
 
-def delete_file_metadata(file_hash):
+def delete_file_metadata(file_hash: str) -> bool:
     """ Удаление метаданных файла """
     metadata = load_metadata()
     if file_hash in metadata:
